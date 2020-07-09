670fc549ad1a08f5ad08fca8b545ef0f115b4df0
Glitch (rick-n) <none>
2020-07-09 06:53:06 +0000

    👩👍 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,39 @@
+//Global Variables
+//This is where you will define the variables you will be using in your project.
+//#TODO: Create four variables to track each possible quiz outcome
+var questionCount = 0;
+var RickScore = 0;
+var MortyScore = 0;
+var SummerScore = 0;
+var JerryScore = 0;
+
+
+
+
+
+//#TODO: Use the DOM to create variables for the first quiz question.
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q13 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+
+
+
+
+
+
+//#TODO: Add Event Listeners to your answer choice variables.
+q1a1.addEventListener("click", result1);
+
+
+
+
+
+
+
+//#TODO: Define quiz functions here
+function result1(){
+  
+  
+  
+}
